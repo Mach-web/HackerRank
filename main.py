5fe7ab69a99d7870b117f79b18fda21ccd5d2e22
Mach-web <mkanderson771@gmail.com>
2023-06-13 15:58:31 +0300

    submitted and running code. Learned using strptime, map, lambda and abs

--- main.py
+++ main.py
@@ -12,57 +12,49 @@ from datetime import *
 def time_delta(t1, t2):
     t1 = t1.split()
     t2 = t2.split()
-    t1[2] = convert_month(t1[2])
-    t2[2] = convert_month(t2[2])
+    # t1[2] = convert_month(t1[2])
+    # t2[2] = convert_month(t2[2])
+    t1_date = "-".join(map(lambda x: x, t1[1:6]))
+    t2_date = " ".join(map(lambda x: x, t2[1:6]))
 
-    t1_date = "-".join(map(lambda x: x, t1[1:5]))
-    t2_date = " ".join(map(lambda x: x, t2[1:5]))
-
-    t1_date = datetime.strptime(t1_date, "%d-%m-%Y-%H:%M:%S")
-    t2_date = datetime.strptime(t2_date, "%d %m %Y %H:%M:%S")
+    t1_date = datetime.strptime(t1_date, "%d-%b-%Y-%H:%M:%S-%z")
+    t2_date = datetime.strptime(t2_date, "%d %b %Y %H:%M:%S %z")
     time_diff = t1_date - t2_date
-    print(time_diff)
     time_diff = time_diff.total_seconds()
-    print(time_diff)
-    t1_zone = (int(t1[-1][:3]) * 3600) + (int(t1[-1][3:]) * 60)
-    t2_zone = (int(t2[-1][:3]) * 3600) + (int(t2[-1][3:]) * 60)
-    timezone_diff = t1_zone - t2_zone
-
-    total_diff = time_diff + timezone_diff
-    return total_diff
-    #413962293
-    #12527392
+    return abs(time_diff)
 def convert_month(month):
     month = month.lower()
-    if month == "january" or "jan":
-        return "1"
-    elif month == "february" or "feb":
-        return "2"
-    elif month == "march" or "mar":
-        return "3"
-    elif month == "april" or "apr":
-        return "4"
+    if month == "january" or month == "jan":
+        return "01"
+    elif month == "february" or month == "feb":
+        return "02"
+    elif month == "march" or month == "mar":
+        return "03"
+    elif month == "april" or month == "apr":
+        return "04"
     elif month == "may":
-        return "5"
-    elif month == "june" or "jun":
-        return "6"
-    elif month == "july" or "jul":
-        return "7"
-    elif month == "august" or "aug":
-        return "8"
-    elif month == "september" or "sep":
-        return "9"
-    elif month in ["october", "oct"]:
+        return "05"
+    elif month == "june" or month == "jun":
+        return "06"
+    elif month == "july" or month == "jul":
+        return "07"
+    elif month == "august" or month == "aug":
+        return "08"
+    elif month == "september" or month == "sep":
+        return "09"
+    elif month == "october" or month == "oct":
         return "10"
-    elif month == "november" or "nov":
+    elif month == "november" or month == "nov":
         return "11"
-    elif month == "december" or "dec":
+    elif month == "december" or month == "dec":
         return "12"
-
+print(time_delta("Sun 10 May 2015 13:54:36 -0700", "Sun 10 May 2015 13:54:36 -0000"))
 print(time_delta("Fri 11 Feb 2078 00:05:21 +0400", "Mon 29 Dec 2064 03:33:48 -1100"))
 print(time_delta("Wed 12 May 2269 23:22:15 -0500", "Tue 05 Oct 2269 02:12:07 -0200"))
+print(time_delta("Sat 14 Sep 2126 00:36:44 +1400", "Wed 22 Jun 2050 23:18:57 -0100"))
 # 413962293
 # 12527392
+#2405413067
 # if __name__ == '__main__':
 #     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 #     t = int(input())
--- timeDelta.py
+++ timeDelta.py
@@ -12,53 +12,20 @@ from datetime import *
 def time_delta(t1, t2):
     t1 = t1.split()
     t2 = t2.split()
+    # t1[2] = convert_month(t1[2])
+    # t2[2] = convert_month(t2[2])
+    t1_date = "-".join(map(lambda x: x, t1[1:6]))
+    t2_date = " ".join(map(lambda x: x, t2[1:6]))
 
-    t1_date = date(int(t1[3]), convert_month(t1[2]), int(t1[1]))
-    t2_date = date(int(t2[3]), convert_month(t2[2]), int(t2[1]))
-    date_difference = t1_date - t2_date
-    date_difference = date_difference.total_seconds()
-
-    time_diff = datetime.strptime(t1[4], "%H:%M:%S") - datetime.strptime(t2[4], "%H:%M:%S")
+    t1_date = datetime.strptime(t1_date, "%d-%b-%Y-%H:%M:%S-%z")
+    t2_date = datetime.strptime(t2_date, "%d %b %Y %H:%M:%S %z")
+    time_diff = t1_date - t2_date
     time_diff = time_diff.total_seconds()
-
-    t1_zone = (int(t1[-1][:3]) * 3600) + (int(t1[-1][3:]) * 60)
-    t2_zone = (int(t2[-1][:3]) * 3600) + (int(t2[-1][3:]) * 60)
-    timezone_diff = t2_zone - t1_zone
-
-    total_diff = date_difference + time_diff + timezone_diff
-    print(total_diff)
-
-
-def convert_month(month):
-    month = month.lower()
-    if month == "january":
-        return 1
-    elif month == "february":
-        return 2
-    elif month == "march":
-        return 3
-    elif month == "april":
-        return 4
-    elif month == "may":
-        return 5
-    elif month == "june":
-        return 6
-    elif month == "july":
-        return 7
-    elif month == "august":
-        return 8
-    elif month == "september":
-        return 9
-    elif month == "october":
-        return 10
-    elif month == "november":
-        return 11
-    elif month == "december":
-        return 12
+    return abs(time_diff)
 
 
 if __name__ == '__main__':
-
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
     t = int(input())
 
     for t_itr in range(t):
@@ -67,7 +34,7 @@ if __name__ == '__main__':
         t2 = input()
 
         delta = time_delta(t1, t2)
+        if delta is not None:
+            fptr.write(f"{delta:.0f}" + "\n")
 
-        print(delta + '\n')
-
-
+    fptr.close()
