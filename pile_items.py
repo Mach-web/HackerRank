28730eb2757b31bdc7703369b7992c0fdb47fa65
Mach-web <mkanderson771@gmail.com>
2023-06-24 18:19:12 +0300

    A dynamic solution instead of a recursion

--- pile_items.py
+++ pile_items.py
@@ -1,36 +1,42 @@
 ''' import numpy - was thinking of converting the whole input to an array but that would not be efficient.
 There is no need to convert all values to integers yet we would not need to evaluate all of them'''
-def pile_items(arr):
-    # an array can definitely form a stack if it has 2 elements
-    if len(arr) == 2:
-        return 'Yes'
+import sys
 
-    if int(arr[-1]) > int(arr[0]):
-        if int(arr[-2]) > int(arr[-1]):
+
+def pile_items(length, arr):
+    for _ in range(0, length):
+        if length <= 2:
+            return 'Yes'
+            break
+        if arr[-1] > arr[0]:
+            if arr[-2] > arr[-1]:
             # Given that the element at the chosen edge was largest, return No if it is smaller than next element
-            return 'No'
-        else:
-            # concat the list to remove the chosen element then perform a recursion.
-            arr = arr[:-1]
-            return pile_items(arr)
+                return 'No'
+                break
+            else:
+            # remove the last element and reduce the length by 1
+                length -= 1
+                arr.pop(-1)
 
-    elif int(arr[0]) >= int(arr[-1]):
-        if int(arr[1]) > int(arr[0]):
-            return 'No'
         else:
-            arr = arr[1:]
-            return pile_items(arr)
+            if arr[1] > arr[0]:
+                return 'No'
+                break
+            else:
+                length -= 1
+                arr.pop(0)
 
 
 if __name__ == '__main__':
     inputs = int(input())
-    list_of_results = []
+    results = []
     for inpt in range(inputs):
         length = int(input())
-        arr = input().split()
-        list_of_results.append(pile_items(arr))
-    for result in list_of_results:
-        print(result, end = '\n')
+        arr = list(map(int, input().split()))
+        results.append(pile_items(length, arr))
+    for result in results:
+        print(result)
+
 """Sample Input
 2
 6
