2b3fdcfe330ee985ba22bad4d64e98cdf8da872f
Mach-web <mkanderson771@gmail.com>
2023-06-29 09:22:18 +0300

    Added link of the problem.

--- matrix_linearAlgebra_numpy.py
+++ matrix_linearAlgebra_numpy.py
@@ -1,12 +1,17 @@
 # Task You are given a square matrix A with dimensions N*N. Your task is to find the determinant. Note: Round the answer to 2 places after the decimal.
+'''
+Link to the problem:
+https://www.hackerrank.com/challenges/np-linear-algebra/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+'''
 import numpy as np
 
 N = int(input())
+# instantiate an empty array
 matrix = np.empty((N, N))
 
 for i in range(N):
     inputs = input().split(" ")
     for index, value in enumerate(inputs):
         matrix[i][index] = float(value)
-        
+# find the determinant
 print(round(np.linalg.det(matrix), 2))
