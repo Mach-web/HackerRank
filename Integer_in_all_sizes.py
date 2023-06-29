30541173292bd4befd3b1e50192c8d8ece4acdc8
Mach-web <mkanderson771@gmail.com>
2023-06-29 09:33:13 +0300

    Integer operations. 10 points.

---
+++ Integer_in_all_sizes.py
@@ -0,0 +1,12 @@
+'''
+link to the problem:
+https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+'''
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+result = (a ** b) + (c ** d)
+print(result)
