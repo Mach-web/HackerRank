5101c7bfdb91b0b827c20e1fae44d127d4688e7c
Mach-web <mkanderson771@gmail.com>
2023-07-05 10:47:13 +0300

    Dynamic solution. Passed test cases.

--- pile_items.py
+++ pile_items.py
@@ -1,40 +1,37 @@
 ''' import numpy - was thinking of converting the whole input to an array but that would not be efficient.
-There is no need to convert all values to integers yet we would not need to evaluate all of them'''
-import sys
-
-
+There is no need to convert all values to integers yet we would not need to evaluate all of them
+https://www.hackerrank.com/challenges/piling-up/problem
+'''
 def pile_items(length, arr):
-    for _ in range(0, length):
-        if len(arr) <= 2:
+    for _ in range(length):
+        # an array can definitely form a stack if it has 2 elements
+        if len(arr) == 2:
             return 'Yes'
-            break
-        if arr[-1] > arr[0]:
-            if arr[-2] > arr[-1]:
+
+        elif int(arr[-1]) > int(arr[0]):
+            if int(arr[-2]) > int(arr[-1]):
             # Given that the element at the chosen edge was largest, return No if it is smaller than next element
                 return 'No'
                 break
             else:
-            # remove the last element and reduce the length by 1
                 arr.pop(-1)
 
-        else:
-            if arr[1] > arr[0]:
+        elif int(arr[0]) >= int(arr[-1]):
+            if int(arr[1]) > int(arr[0]):
                 return 'No'
                 break
             else:
                 arr.pop(0)
 
-
 if __name__ == '__main__':
     inputs = int(input())
-    results = []
+    list_of_results = []
     for inpt in range(inputs):
         length = int(input())
-        arr = list(map(int, input().split()))
-        results.append(pile_items(length, arr))
-    for result in results:
-        print(result)
-
+        arr = list(input().split())
+        list_of_results.append(pile_items(length, arr))
+    for result in list_of_results:
+        print(result, end = '\n')
 """Sample Input
 2
 6
@@ -45,3 +42,8 @@ Output
 Yes
 No
 """
+''' import numpy - was thinking of converting the whole input to an array but that would not be efficient.
+There is no need to convert all values to integers yet we would not need to evaluate all of them'''
+
+
+
