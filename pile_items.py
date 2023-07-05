c5b047463aa5652f614e8638dbb81b955f662567
Mach-web <mkanderson771@gmail.com>
2023-07-05 10:35:28 +0300

    Recursive soln. Dynamic solution is found in dynamic branch

--- pile_items.py
+++ pile_items.py
@@ -1,34 +1,35 @@
 ''' import numpy - was thinking of converting the whole input to an array but that would not be efficient.
 There is no need to convert all values to integers yet we would not need to evaluate all of them'''
-def pile_items(arr):
+def pile_items(length, arr):
     # an array can definitely form a stack if it has 2 elements
-    if len(arr) == 2:
+
+    if length == 2:
         return 'Yes'
 
-    if int(arr[-1]) > int(arr[0]):
+    elif int(arr[-1]) > int(arr[0]):
         if int(arr[-2]) > int(arr[-1]):
             # Given that the element at the chosen edge was largest, return No if it is smaller than next element
             return 'No'
         else:
-            # concat the list to remove the chosen element then perform a recursion.
-            arr = arr[:-1]
-            return pile_items(arr)
+            length -= 1
+            arr.pop(-1)
+            return pile_items(length, arr)
 
     elif int(arr[0]) >= int(arr[-1]):
         if int(arr[1]) > int(arr[0]):
             return 'No'
         else:
-            arr = arr[1:]
-            return pile_items(arr)
-
+            length -= 1
+            arr.pop(0)
+            return pile_items(length, arr)
 
 if __name__ == '__main__':
     inputs = int(input())
     list_of_results = []
     for inpt in range(inputs):
         length = int(input())
-        arr = input().split()
-        list_of_results.append(pile_items(arr))
+        arr = list(input().split())
+        list_of_results.append(pile_items(length, arr))
     for result in list_of_results:
         print(result, end = '\n')
 """Sample Input
@@ -41,3 +42,8 @@ Output
 Yes
 No
 """
+''' import numpy - was thinking of converting the whole input to an array but that would not be efficient.
+There is no need to convert all values to integers yet we would not need to evaluate all of them'''
+
+
+
