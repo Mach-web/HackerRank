ed4e1614f7a168b9000048b5a83f367270408184
Mach-web <mkanderson771@gmail.com>
2023-07-06 09:37:04 +0300

    passed test cases. Updated question link.10 points.

--- setUnion_Operation.py
+++ setUnion_Operation.py
@@ -1,6 +1,9 @@
+"""https://www.hackerrank.com/challenges/py-set-union/problem?h_r=next-challenge&h_v=zen"""
 if __name__ == "__main__":
     len_english = int(input())
     english = set(input().split())
+
     len_french = int(input())
     french = set(input().split())
+    # perform a union and output the results
     print(len(english.union(french)))
