96014acbe35bf66753da9b6311a82e128e6b4813
login <mkanderson771@gmail.com>
2023-08-16 16:09:36 +0300

    Added comments

---
+++ find_percentage.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    scores = student_marks[query_name]
+    sum_scores = 0
+    length = 0
+    for _ in scores:
+        sum_scores += _
+        length += 1
+    avg = sum_scores/length
+    print("{:.2f}".format(avg))
