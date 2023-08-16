f9d9e70ff17341fdda604aeb3ab4dff81452b46b
login <mkanderson771@gmail.com>
2023-08-16 16:10:21 +0300

    Added comments

--- find_percentage.py
+++ find_percentage.py
@@ -9,8 +9,10 @@ if __name__ == '__main__':
     scores = student_marks[query_name]
     sum_scores = 0
     length = 0
+    # Find the average
     for _ in scores:
         sum_scores += _
         length += 1
     avg = sum_scores/length
+    # print with 2 decimal places
     print("{:.2f}".format(avg))
