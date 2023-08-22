7196f089f1b14d484f0d8c6f3d5151b8c8fcacf0
login <mkanderson771@gmail.com>
2023-08-22 17:37:22 +0300

    How to use __getattribute__ to retrieve a function

---
+++ Lists.py
@@ -0,0 +1,9 @@
+import importlib
+if __name__ == '__main__':
+    # N = int(input())
+    myList = []
+    # for inputs in N:
+    #     inputs.
+    func = 'append'
+    myList.__getattribute__(func)(1)
+    print(myList)
