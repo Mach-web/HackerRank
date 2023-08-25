752badff9cc5264c85974f6d470e1ca27410b003
login <mkanderson771@gmail.com>
2023-08-25 12:02:06 +0300

    Passed all test cases

--- sWAP_cASE.py
+++ sWAP_cASE.py
@@ -3,6 +3,7 @@ https://www.hackerrank.com/challenges/swap-case/problem?isFullScreen=true
 '''
 def swap_case(s):
     letter_list = [letter.lower() if letter.isupper() else letter.upper() for letter in s]
+    # Join the list to a sr=tring
     return ''.join(letter_list)
 
 if __name__ == "__main__":
