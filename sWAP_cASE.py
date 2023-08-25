c55dcefba3092b9b68b4f9d961d3249c71ae244e
login <mkanderson771@gmail.com>
2023-08-25 11:58:40 +0300

    Added test link

--- sWAP_cASE.py
+++ sWAP_cASE.py
@@ -1,8 +1,11 @@
+'''
+https://www.hackerrank.com/challenges/swap-case/problem?isFullScreen=true
+'''
 def swap_case(s):
     letter_list = [letter.lower() if letter.isupper() else letter.upper() for letter in s]
     return ''.join(letter_list)
 
-if "__main__" == __name__:
+if __name__ == "__main__":
     s = input()
     result = swap_case(s)
     print(result)
