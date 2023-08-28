f89ecae33b6ebe1017f9427bd378be2a9f847d81
login <mkanderson771@gmail.com>
2023-08-28 17:31:58 +0300

    one of the implementations of decimal to binary.

--- String_Formatting.py
+++ String_Formatting.py
@@ -1,5 +1,21 @@
 def print_formatted(number):
-    # your code goes here
+    if number != 1:
+        print_formatted(number // 2)
+
+    print(number % 2)
+def convert_to_hexa(s):
+    if s == 10:
+        return "A"
+    elif s == 11:
+        return "B"
+    elif s == 12:
+        return "C"
+    elif s == 13:
+        return "D"
+    elif s == 14:
+        return "E"
+    else:
+        return "F"
 
 if __name__ == '__main__':
     n = int(input())
