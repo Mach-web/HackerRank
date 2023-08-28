3e71dccd153c67547c3bd9e2ab76c538f0bbbfa2
login <mkanderson771@gmail.com>
2023-08-28 10:29:54 +0300

    Passed test cases. 10 points.

--- Capitalize.py
+++ Capitalize.py
@@ -1,7 +1,11 @@
+"""https://www.hackerrank.com/challenges/capitalize/problem?isFullScreen=true"""
 def solve(s):
+    # print(s.split(" "))
     capitalized_s = []
-    for i in s.split():
+    # one has to account for the case of several spaces
+    for i in s.split(" "):
         capitalized_s.append(i.capitalize())
+    # print(capitalized_s)
     return " ".join(capitalized_s)
 
 if __name__ == "__main__":
