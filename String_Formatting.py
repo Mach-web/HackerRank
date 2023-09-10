1d45a4e2f2189d0f51ffee37b34c20fdb81efa27
login <mkanderson771@gmail.com>
2023-09-10 06:49:48 +0300

    Works perfectly apart from the fact that formatting is not well done.

--- String_Formatting.py
+++ String_Formatting.py
@@ -1,21 +1,33 @@
-def print_formatted(number):
-    if number != 1:
-        print_formatted(number // 2)
+def decimal(n):
+    return str(n)
+def binary(number):
+    bin = []
+    while number != 0:
+        bin.append(str(number%2))
+        number = number//2
+    bin.reverse()
+    return ''.join(bin)
 
-    print(number % 2)
-def convert_to_hexa(s):
-    if s == 10:
-        return "A"
-    elif s == 11:
-        return "B"
-    elif s == 12:
-        return "C"
-    elif s == 13:
-        return "D"
-    elif s == 14:
-        return "E"
-    else:
-        return "F"
+def octal(number):
+    octal = []
+    while number != 0:
+        octal.append(str(number % 8))
+        number = number // 8
+    octal.reverse()
+    return ''.join(octal)
+
+def hexa(number):
+    hexa = []
+    while number != 0:
+        hexa.append(str(number % 16))
+        number = number // 16
+    hexa.reverse()
+    hexa = list(map(lambda x: "A" if x == "10" else "B" if x == "11" else "C" if x == "12" else "D" if x == "13" else "E" if x == "14" else "F" if x == "15" else x, hexa))
+    return "".join(hexa)
+
+def print_formatted(n):
+    for i in range(0, n+1):
+        print(decimal(i), octal(i), hexa(i), binary(i))
 
 if __name__ == '__main__':
     n = int(input())
