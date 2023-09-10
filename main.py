feea5bf4d0b381a1a273c4c58d09b47b40df5b52
login <mkanderson771@gmail.com>
2023-09-10 06:48:48 +0300

    Int to string.

--- main.py
+++ main.py
@@ -10,14 +10,10 @@
 # if b == 0:
 #     raise "Error found"
 
-def hexa(number):
-    hexa = []
-    while number != 0:
-        hexa.append(str(number % 16))
-        number = number // 16
-    hexa.reverse()
-    hexa = list(map(lambda x: "A" if x == "10" else "B" if x == "11" else "C" if x == "12" else "D" if x == "13" else "E" if x == "14" else "F" if x == "15" else x, hexa))
-    return "".join(hexa)
+def decimal(n):
+    return str(n)
+
+print(decimal(12))
 
 
 
