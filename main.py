2c95490e2b497ce14f4e13fd839daf7fa9c13783
login <mkanderson771@gmail.com>
2023-09-10 06:32:31 +0300

    Implementation of converting deciaml number to hexadecimal.

--- main.py
+++ main.py
@@ -9,14 +9,31 @@
 # b = int(val[1])
 # if b == 0:
 #     raise "Error found"
-
-print(2//8)
-print(2%8)
+def convert_to_hexa(s):
+    if s == '10':
+        return "A"
+    elif s == '11':
+        return "B"
+    elif s == '12':
+        return "C"
+    elif s == '13':
+        return "D"
+    elif s == '14':
+        return "E"
+    elif s == '15':
+        return "F"
+    else:
+        return s
 def octal(number):
     octal = []
     while number != 0:
-        octal.append(str(number % 8))
-        number = number // 8
+        octal.append(str(number % 16))
+        number = number // 16
     octal.reverse()
-    return ''.join(octal)
-print(octal(14))
+    octal = list(map(lambda x: "A" if x == "10" else "B" if x == "11" else "C" if x == "12" else "D" if x == "13" else "E" if x == "14" else "F" if x == "15" else x, octal))
+    print(octal)
+
+octal(16)
+print(14 // 16)
+
+
